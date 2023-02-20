3d510fd3a9f9195f4ba8decd7a90336274c27129
am-shm <am.shahmoradi93@gmail.com>
2023-02-20 15:28:16 +0000

    install script added: base menu options and directories created

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Directories
+mkdir $HOME/.config
+mkdir $HOME/.config/ocgui
+mkdir $HOME/.config/ocgui/menu
+mkdir $HOME/.config/ocgui/profile
+
+# Files
+touch $HOME/.config/ocgui/menu/Connect
+touch $HOME/.config/ocgui/menu/Create\ Profile
+touch $HOME/.config/ocgui/menu/Edit\ Profile
